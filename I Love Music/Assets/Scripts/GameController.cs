cfb0d783143b255b3725770586c31bea64f2e23a
DIGITALSTORM-PC\Admin <jxw7456@g.rit.edu>
2019-02-10 18:17:49 -0500

    polishing game

--- I Love Music/Assets/Scripts/GameController.cs
+++ I Love Music/Assets/Scripts/GameController.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -16,6 +15,7 @@ public class GameController : MonoBehaviour
     public GameObject questionDisplay, roundEndDisplay;
 
     private DataController dataController;
+
     private RoundData currentRoundData;
 
     private QuestionData[] questionPool;
@@ -46,23 +46,27 @@ public class GameController : MonoBehaviour
         isRoundActive = true;
     }
 
+    // Grabs the proper Question Pool depending on the artist and displays their questions
     private void ShowQuestion()
     {
+        // Remove all answer buttons being used
         RemoveAnswerButtons();
         QuestionData questionData = questionPool[questionIndex];
         questionText.text = questionData.questionText;
 
+        // Add the optional answers to the GameObject and set them up
         for (int i = 0; i < questionData.answers.Length; i++)
         {
             GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();
             answerButtonGameObjects.Add(answerButtonGameObject);
-            answerButtonGameObject.transform.SetParent(answerButtonParent);            
+            answerButtonGameObject.transform.SetParent(answerButtonParent);
 
             AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
             answerButton.SetUp(questionData.answers[i]);
         }
     }
 
+    // Return all answer buttons back to the pool for reuse
     private void RemoveAnswerButtons()
     {
         while (answerButtonGameObjects.Count > 0)
@@ -72,6 +76,7 @@ public class GameController : MonoBehaviour
         }
     }
 
+    // Check if the button click contains the correct answer
     public void AnswerButtonClicked(bool isCorrect)
     {
         if (isCorrect)
@@ -79,8 +84,17 @@ public class GameController : MonoBehaviour
             roundScore += currentRoundData.pointsAddedForCorrectAnswer;
             playerScore = roundScore;
             scoreDisplayText.text = "Score: " + playerScore.ToString();
-        }            
+        }
+        else
+        {
+            // Set the entire player's score back to zero
+            playerScore = 0;
+            PlayerPrefs.SetInt("Score", 0);
+            roundScore = PlayerPrefs.GetInt("Score");
+            scoreDisplayText.text = "Score: " + playerScore.ToString();
+        }
 
+        // Check if the the player still has more questions
         if (questionPool.Length > questionIndex + 1)
         {
             questionIndex++;
@@ -90,6 +104,7 @@ public class GameController : MonoBehaviour
             EndRound();
     }
 
+    // Sets the round to inactive and takes in the player's round score
     public void EndRound()
     {
         isRoundActive = false;
@@ -100,11 +115,13 @@ public class GameController : MonoBehaviour
         PlayerPrefs.SetInt("Score", roundScore);
     }
 
+    // Returns the player back to the select scene
     public void ReturnToArtists()
     {
         SceneManager.LoadScene("SelectScene");
     }
 
+    // Constantly updates the time remaining for the player
     private void UpdateTimeRemainingDisplay()
     {
         timeRemainingDisplayText.text = "Time: " + Mathf.Round(timeRemaining).ToString();
@@ -115,9 +132,11 @@ public class GameController : MonoBehaviour
     {
         if (isRoundActive)
         {
+            // Countdown timer
             timeRemaining -= Time.deltaTime;
             UpdateTimeRemainingDisplay();
 
+            // Ends the round once time is up
             if (timeRemaining <= 0f)
                 EndRound();
         }
